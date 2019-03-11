9793dbd7aa517df59ba39e6c52df403336124576
Bana <banabatshon@hotmail.com>
2019-03-11 19:30:28 +0000

    creats a callback funtion that prints the html data in lower case after it is required

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,11 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printHTMLToLowerCase (html) {
+  console.log((html.toLowerCase()));
+};
+console.log(getHTML(requestOptions, printHTMLToLowerCase));
